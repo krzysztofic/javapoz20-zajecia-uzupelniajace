f8ce7b8e0757dc3cdd19c9a07c628136f015a0e7
krzysztofic <krzysztof.iciek@gmail.com>
2020-05-16 11:22:40 +0200

    Threads #2 - synchronizing of increment method

--- src/main/java/com/sda/singleton/CountAction.java
+++ src/main/java/com/sda/singleton/CountAction.java
@@ -12,6 +12,11 @@ public class CountAction implements Runnable {
     public void run() {
 
         for (int i = 0; i < 100000 ; i++) {
+            try{
+                Thread.sleep(100);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             countService.increment();
         }
     }
--- src/main/java/com/sda/singleton/CountService.java
+++ src/main/java/com/sda/singleton/CountService.java
@@ -15,7 +15,7 @@ public class CountService {
     private CountService(){
     }
 
-    public void increment(){
+    public synchronized void increment(){
         this.count = this.count + 1;
     }
 
