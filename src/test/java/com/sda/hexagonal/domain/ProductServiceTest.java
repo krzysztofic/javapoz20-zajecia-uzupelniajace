6e562adc3fdc60ab149593a8a8e566871947daf1
krzysztofic <krzysztof.iciek@gmail.com>
2020-05-17 14:01:14 +0200

    Hexagonal #4 - use of mock instead of fake

--- src/test/java/com/sda/hexagonal/domain/ProductServiceTest.java
+++ src/test/java/com/sda/hexagonal/domain/ProductServiceTest.java
@@ -1,7 +1,11 @@
 package com.sda.hexagonal.domain;
 
+import com.sda.hexagonal.domain.port.ProductRepository;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
 
 
 public class ProductServiceTest {
@@ -10,11 +14,25 @@ public class ProductServiceTest {
     public void shouldCreateProduct() {
 
         //given
-        ProductService productService = new ProductService(new FakeProductRepository());
         String name = "product-name";
         String description = "product-description";
         ProductCategory category = ProductCategory.PHONE;
         Double weight = 1.5;
+
+        ProductRepository productRepositoryMock = Mockito.mock(ProductRepository.class);
+        Mockito.when(productRepositoryMock.findByCategory(ProductCategory.PHONE))
+                .then(e -> Arrays.asList(
+                        new Product(1L, "product-1", "product-description-1", ProductCategory.PHONE, 1.5),
+                        new Product(2L, "product-3", "product-description-3", ProductCategory.PHONE, 1.2),
+                        new Product(3L, "product-8", "product-description-8", ProductCategory.PHONE, 1.8),
+                        new Product(4L, "product-15", "product-description-15", ProductCategory.PHONE, 2.5)
+                ));
+
+        Mockito.when(productRepositoryMock.save(Mockito.any()))
+                .then(e -> new Product(1L, name, description, category, weight));
+
+        ProductService productService = new ProductService(productRepositoryMock);
+
 
         //when
         Product actualProduct = productService.addProduct(name, description, category, weight);
@@ -28,18 +46,20 @@ public class ProductServiceTest {
 
 
     @Test(expected = ProductAlreadyExistsException.class)
-    public void shouldThrowProductAlreadyExistExceptionWhileTryingToCreateProductWithSameName(){
-        //given
+    public void shouldThrowProductAlreadyExistsExceptionWhileTryingToCreateProductWithSameName() {
+        // given
+        ProductRepository productRepositoryMock = Mockito.mock(ProductRepository.class);
+        Mockito.when(productRepositoryMock.findByCategory(Mockito.any()))
+                .then(e -> Arrays.asList(new Product(1L, "duplicated-name", null, null, null)));
 
-        ProductService productService = new ProductService(new FakeProductRepository());
+        ProductService productService = new ProductService(productRepositoryMock);
 
-            String name = "product-1";
-            String description = "product-description";
-            ProductCategory category = ProductCategory.PHONE;
-            Double weight = 1.5;
+        String name = "duplicated-name";
+        String description = "product-description";
+        ProductCategory category = ProductCategory.PHONE;
+        Double weight = 1.5;
 
-        //when
+        // when
         productService.addProduct(name, description, category, weight);
-        }
-
     }
+}
