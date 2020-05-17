173da4a8d88a51459b4aa43815b6a73e2d77b0d2
krzysztofic <krzysztof.iciek@gmail.com>
2020-05-17 13:38:35 +0200

    Hexagonal #3 - additional test for exception thrown

--- src/test/java/com/sda/hexagonal/domain/ProductServiceTest.java
+++ src/test/java/com/sda/hexagonal/domain/ProductServiceTest.java
@@ -7,24 +7,39 @@ import org.junit.Test;
 public class ProductServiceTest {
 
     @Test
-    public void shouldCreateProduct(){
+    public void shouldCreateProduct() {
 
-    //given
-    ProductService productService = new ProductService(new FakeProductRepository());
-    String name = "product-name";
-    String description = "product-description";
-    ProductCategory category = ProductCategory.PHONE;
-    Double weight = 1.5;
+        //given
+        ProductService productService = new ProductService(new FakeProductRepository());
+        String name = "product-name";
+        String description = "product-description";
+        ProductCategory category = ProductCategory.PHONE;
+        Double weight = 1.5;
 
-    //when
-    Product actualProduct = productService.addProduct(name, description, category, weight);
-
-
-    //then
-    Product expectedProduct = new Product(1L, name, description, category, weight);
+        //when
+        Product actualProduct = productService.addProduct(name, description, category, weight);
 
 
-    Assert.assertEquals("Product is different", expectedProduct, actualProduct);
+        //then
+        Product expectedProduct = new Product(1L, name, description, category, weight);
+
+        Assert.assertEquals("Product is different", expectedProduct, actualProduct);
+    }
+
+
+    @Test(expected = ProductAlreadyExistsException.class)
+    public void shouldThrowProductAlreadyExistExceptionWhileTryingToCreateProductWithSameName(){
+        //given
+
+        ProductService productService = new ProductService(new FakeProductRepository());
+
+            String name = "product-1";
+            String description = "product-description";
+            ProductCategory category = ProductCategory.PHONE;
+            Double weight = 1.5;
+
+        //when
+        productService.addProduct(name, description, category, weight);
+        }
 
     }
-}
